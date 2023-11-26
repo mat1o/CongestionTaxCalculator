1848d7090eba815cc2c1b65e5c15717e7e4bc92c
erfani_m <erfani_m@HQ-DEV1>
2023-11-26 11:23:11 +0330

    Add fixture and Configurations for xUnit Test.

---
+++ CongestionTaxCalculator.xUnitTest/ServiceFixture.cs
@@ -0,0 +1,26 @@
+﻿using CongestionTaxCalculator.Domain.Context;
+using CongestionTaxCalculator.Service.Services.Abstraction;
+using CongestionTaxCalculator.Service.Services.Implementation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CongestionTaxCalculator.xUnitTest
+{
+    public class ServiceFixture
+    {
+        public ServiceFixture()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddDbContext<ApplicationDbContext>(options =>
+                 options.UseSqlServer(""), ServiceLifetime.Scoped);
+
+            serviceCollection.AddScoped<ITollService, TollService>();
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+        }
+
+        public ServiceProvider ServiceProvider { get; set; }
+    }
+}
--- CongestionTaxCalculator.xUnitTest/ServiceTest/TaxServiceTest.cs
+++ CongestionTaxCalculator.xUnitTest/ServiceTest/TaxServiceTest.cs
@@ -1,12 +1,19 @@
 ﻿using CongestionTaxCalculator.Service.Services.Abstraction;
 using CongestionTaxCalculator.Service.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 
 namespace CongestionTaxCalculator.xUnitTest.ServiceTest
 {
-    public class TaxServiceTest
+    
+    public class TaxServiceTest : IClassFixture<ServiceFixture>
     {
-        private readonly ITollService _tollService;
+        private ServiceProvider _serviceProvider;
+
+        public TaxServiceTest(ServiceFixture serviceFixture)
+        {
+            _serviceProvider = serviceFixture.ServiceProvider;
+        }
 
         public static IEnumerable<object[]> SplitCountData =>
         new List<object[]>
@@ -15,17 +22,14 @@ namespace CongestionTaxCalculator.xUnitTest.ServiceTest
             new object[] { new CityViewModel { Name = "Berlin" }, new TimeOnly(15,00,00) , 18.0f }, //fasls. Berlin's toll data is not available.
             new object[] { new CityViewModel { Name = "Gothenburg" }, new TimeOnly(21,30,00) , 12.20f }, // false. during 18:30 to 05:59 toll is free.
         };
-
-        public TaxServiceTest(ITollService tollService)
-        {
-            _tollService = tollService;
-        }
-
+        
 
         [Theory, MemberData(nameof(SplitCountData))]
         public void CalculateToll(CityViewModel city, TimeOnly time, float expectedToll)
         {
-            var toll = _tollService.CalculatTollForTest(city, time);
+            var tollService = _serviceProvider.GetService<ITollService>();
+
+            var toll = tollService?.CalculatTollForTest(city,time);
 
             Assert.Equal(toll, expectedToll);
         }
