6d7305f4b96570ded0b4fd08ee4f8060fcd1547b
erfani_m <erfani_m@HQ-DEV1>
2023-11-26 12:05:22 +0330

    .

--- CongestionTaxCalculator.xUnitTest/ServiceTest/TaxServiceTest.cs
+++ CongestionTaxCalculator.xUnitTest/ServiceTest/TaxServiceTest.cs
@@ -19,9 +19,9 @@ namespace CongestionTaxCalculator.xUnitTest.ServiceTest
         new List<object[]>
         {
             new object[] { new CityViewModel { Name = "Gothenburg" }, new TimeOnly(16,00,00) , 18.0f }, //true
-            new object[] { new CityViewModel { Name = "Berlin" }, new TimeOnly(15,00,00) , 18.0f }, //fasls. Berlin's toll data is not available.
+            new object[] { new CityViewModel { Name = "Berlin" }, new TimeOnly(15,00,00) , 18.0f }, //false. Berlin's toll data is not available.
             new object[] { new CityViewModel { Name = "Gothenburg" }, new TimeOnly(21,30,00) , 12.20f }, // false. during 18:30 to 05:59 toll is free.
-            new object[] { new CityViewModel { Name = "London" }, new TimeOnly(13, 00, 00), 15.5f }, // false. during 18:30 to 05:59 toll is free.
+            new object[] { new CityViewModel { Name = "London" }, new TimeOnly(13, 00, 00), 15.5f }, // true.
         };
 
         [Theory, MemberData(nameof(SplitCountData))]
